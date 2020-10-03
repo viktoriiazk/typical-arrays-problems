9c18e6e9f0017a5703bd5b7227b905373590d48f
Viktoriia Zakorchemna <viktoriia.zakorchemna@gmail.com>
2020-10-03 10:33:37 +0300

    feat: implement min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 
 exports.min = function min (array) {
+  if (array && array.length !== 0){
+    return  Math.min(...array);  
+ }
   
-  return 0;
+   return 0;
+ 
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array && array.length !== 0){
+    return  Math.max(...array);  
+ }
+   return 0;
 }
 
 exports.avg = function avg (array) {
+  if (array && array.length !== 0) {
+    let total = 0;
+    for(let i = 0; i < array.length; i++) {
+        total += array[i];
+    }
+    let avg = total / array.length;
+    return avg;
+  }
   return 0;
 }
